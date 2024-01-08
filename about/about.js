044b717fba8723f51ead83d3641cc16556c7a32a
maxschgol <maxschgol@hotmail.com>
2024-01-08 13:27:42 +0100

    creo que ya

--- about/about.js
+++ about/about.js
@@ -9,21 +9,3 @@
     // event
     menu.addEventListener('click', toggleMenu, false);
 
-
-
-    //Imagen principal
-    gsap.registerPlugin(ScrollTrigger);
-
-    let element = document.querySelector(".img1");
-    gsap.set(element, { y: -2000,
-   
-    });
-    
-   gsap.to(".img1",{
-    ScrollTrigger:{
-      trigger: ".c",
-      toggleActions: "restart pause reverse pause"
-    },
-    y:0,
-    duration:1
-   })
